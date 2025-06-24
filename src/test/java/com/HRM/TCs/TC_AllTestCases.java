522cceacb851834581924223f8a6118f153655f9
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2025-06-24 23:37:34 +0530

    extent report and page factory with framework

--- src/test/java/com/HRM/TCs/TC_AllTestCases.java
+++ src/test/java/com/HRM/TCs/TC_AllTestCases.java
@@ -2,15 +2,20 @@ package com.HRM.TCs;
 
 import org.testng.annotations.Test;
 
-import base.Generic;
 import com.Orange.pages.HRM_CommonPage;
 import com.Orange.pages.HRM_LoginPage;
 
+import base.Generic;
+
 public class TC_AllTestCases extends Generic {
+	
+//	WebDriver driver;
 	
 	@Test
 	public void validateAPage() throws Exception {
+		Generic.extentLogger= Generic.extentReport.createTest("Validate A Page");
 		HRM_LoginPage loginPage = new HRM_LoginPage(driver);
+//		HRM_LoginPage loginPage = new HRM_LoginPage(getDriver());
 		loginPage.loginOrangeHRMPage("Admin", "admin123");
 		HRM_CommonPage commonPage = new HRM_CommonPage(driver);
 		commonPage.getUserName();
